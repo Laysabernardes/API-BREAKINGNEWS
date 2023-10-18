b55bf25ecc21bd607b3ed8b1204138cdbdc42991
Laysa Bernardes <laysabernardes@gmail.com>
2023-10-18 16:55:52 -0300

    Adicionei os comentarios do User.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,11 +1,15 @@
 import User from "../models/User.js";
 
+//.create é um metodo do mongoose para criar um novo documento no mongoDB
 const createService = (body) => User.create(body);
 
+//.find  é um método de consulta do Mongoose para consultar as coleções
 const findAllService = () => User.find();
 
+//findById é um método de consulta do Mongoose que é utilizado para recuperar um único documento com base no ID.
 const findByIdService = (id) => User.findById(id);
 
+//findOneAndUpdate é um método do Mongoose que permite procurar um documento com base em critérios de consulta 
 const updateService = (id,name,username,email,password,avatar,background) => User.findOneAndUpdate({_id: id},{name,username,email,password,avatar,background});
 //findOneAndUpdate vai procurar por um campo e atualizar
 //_id é o id do mongodb e id é o daqui
