a23dbb3099f7b45c19e148089dee20b23fab6503
Laysa Bernardes <laysabernardes.ads@gmail.com>
2023-12-13 23:46:28 -0300

    CREATE (news schema)(models)

---
+++ src/models/News.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const NewsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true,
+    },
+    text: {
+        type: String,
+        require: true,
+    },
+    banenr: {
+        type: String,
+        require: true,
+    },
+    createdAT: {
+        type: Date,
+        defaut: Date.now(), //Default é para pegar o valor padrão, e date.now pega a hora atual
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // a referencia aqui tem que ser igual a referencia do schema que to querendo pegar, no caso o user
+        require: true, 
+    },
+    likes: {
+        type: Array,
+        require: true,
+    }, 
+    comments: {
+        type: Array,
+        require: true,
+    }
+});
+
+const News = mongoose.model ("News", NewsSchema);
+
+export default News;
