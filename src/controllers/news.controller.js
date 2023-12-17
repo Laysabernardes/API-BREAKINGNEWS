71d3f5f8a0596251c1875784a52e643010add04e
Laysa Bernardes <laysabernardes.ads@gmail.com>
2023-12-16 23:45:10 -0300

    CREATE (find by id em news)(newa controller/service/route)

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,7 +1,7 @@
 import News from "../models/News.js";
-import { createService, findAllService, countNews, topNewsService } from "../services/news.service.js";
+import { createService, findAllService, countNews, topNewsService, findByIdService } from "../services/news.service.js";
 
-const create = async (req, res) => {
+export const create = async (req, res) => {
     try {
         const { title, text, banner } = req.body;
 
@@ -21,7 +21,7 @@ const create = async (req, res) => {
         res.status(500).send({ message: err.mensage })
     }
 }
-const findAll = async (req, res) => {
+export const findAll = async (req, res) => {
 
     try {
         let { limit, offset } = req.query;
@@ -74,8 +74,7 @@ const findAll = async (req, res) => {
     }
 
 }
-
-const topNews = async (req, res) => {
+export const topNews = async (req, res) => {
     try {
         const news = await topNewsService();
 
@@ -101,5 +100,27 @@ const topNews = async (req, res) => {
     }
 
 };
+export const findById = async (req, res) => {
+    try {
+       const { id }= req.params;
 
-export { create, findAll, topNews };
+       const news = await findByIdService(id);
+       
+       return res.send({
+        news: {
+            id: news._id,
+            title: news.title,
+            text: news.text,
+            banner: news.banner,
+            likes: news.likes,
+            comments: news.comments,
+            name: news.user.name,
+            username: news.user.username,
+            userAvatar: news.user.avatar
+        }
+       })
+    } catch (err) {
+        res.status(500).send({ message: err.mensage })
+    }
+}
+
