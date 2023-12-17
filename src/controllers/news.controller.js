0664b010d73b5db72791fa3d1f7f014432da55eb
Laysa Bernardes <laysabernardes.ads@gmail.com>
2023-12-16 22:22:48 -0300

    CREATE (logica de paginação backend)(news controller/service)

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,11 +1,11 @@
-import { createService, findAllService } from "../services/news.service.js";
+import { createService, findAllService, countNews } from "../services/news.service.js";
 
 const create = async (req, res) => {
     try {
         const { title, text, banner } = req.body;
 
-        if(!title || !text || !banner){
-            res.status(400).send({ message: "Adicione todos os elementos!" }); 
+        if (!title || !text || !banner) {
+            res.status(400).send({ message: "Adicione todos os elementos!" });
         }
 
         await createService({
@@ -21,12 +21,52 @@ const create = async (req, res) => {
     }
 }
 const findAll = async (req, res) => {
-    const news = await findAllService();
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+        limit = 5;
+    }
+
+    if (!offset) {
+        offset = 0;
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentURL = req.baseUrl;
+    console.log(currentURL);
+
+    const next = offset + limit;
+    const nextURL = next < total ? `${currentURL}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl = previous != null ? `${currentURL}?limit=${limit}&offset=${previous}` : null;
 
     if (news.length === 0) {
         return res.status(400).send({ message: "Não há postagens registradas." });
     }
-    res.send(news);
+    res.send({
+        nextURL,
+        previous,
+        limit,
+        offset,
+        total,
+
+        result: news.map(item => ({
+            id: item._id,
+            title: item.title,
+            text: item.text,
+            banner: item.banner,
+            likes: item.likes,
+            comments: item.comments,
+            name: item.user.name,
+            username: item.user.username,
+            userAvatar: item.user.avatar
+        }))
+    });
 }
 
 export { create, findAll };
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -2,9 +2,14 @@ import News from '../models/News.js';
 
 const createService = (body) => News.create(body);
 
-const findAllService = () => News.find();
+const findAllService = (offset, limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate("user");
+//populate -> mongoose 
+
+const  countNews =  () => News.countDocuments();
+//countDocuments -> mongoose 
 
 export {
     createService,
-    findAllService
+    findAllService,
+    countNews
 };
